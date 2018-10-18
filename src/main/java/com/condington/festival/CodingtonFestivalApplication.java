621f91d5f17303dc686b4fbc5fa2b7337b19b8b6
Charles Jones <charlescodes@users.noreply.github.com>
2018-10-18 16:19:15 -0500

    commit message

---
+++ src/main/java/com/condington/festival/CodingtonFestivalApplication.java
@@ -0,0 +1,12 @@
+package com.condington.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingtonFestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingtonFestivalApplication.class, args);
+	}
+}
