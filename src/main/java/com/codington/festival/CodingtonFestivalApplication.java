054806ff8b47612ae9328970ee0c0784724a3cb8
Charles Jones <charlescodes@users.noreply.github.com>
2018-10-19 07:19:52 -0500

    correct typos

--- src/main/java/com/codington/festival/CodingtonFestivalApplication.java
+++ src/main/java/com/codington/festival/CodingtonFestivalApplication.java
@@ -1,4 +1,4 @@
-package com.condington.festival;
+package com.codington.festival;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
