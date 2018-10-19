6e5675271baea78ff1dc3ecff196dbfe66c478c1
DanielC1342 <daniel.conner1342@gmail.com>
2018-10-19 15:17:52 -0500

    Update src/main/java/com/codington/festival/Models/Ticket.java

---
+++ src/main/java/com/codington/festival/Models/Ticket.java
@@ -0,0 +1,42 @@
+package com.codington.festival.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Tickets")
+public class Ticket {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "TicketNum", nullable = false, unique = true)
+	private int ticketNum;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private user user_id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTicketNum() {
+		return ticketNum;
+	}
+
+	public void setTicketNum(int ticketNum) {
+		this.ticketNum = ticketNum;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+}
