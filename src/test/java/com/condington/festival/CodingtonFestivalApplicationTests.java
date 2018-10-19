8be6ed05506513d18ad83b0db1b83190488fdecf
Allen <christopher.h.allen@accenturefederal.com>
2018-10-19 10:06:59 -0500

    added registration form

--- src/test/java/com/condington/festival/CodingtonFestivalApplicationTests.java
+++ src/test/java/com/condington/festival/CodingtonFestivalApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.codington.festival.CodingtonFestivalApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=CodingtonFestivalApplication.class)
 public class CodingtonFestivalApplicationTests {
 
 	@Test
