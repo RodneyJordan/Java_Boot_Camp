3eba95624a04c47868c22d7bf41bbdb3d9563e85
Charles Jones <charlescodes@users.noreply.github.com>
2018-10-22 10:19:37 -0500

    make it better 3

--- src/main/java/com/codington/festival/Controllers/UserController.java
+++ src/main/java/com/codington/festival/Controllers/UserController.java
@@ -1,59 +1,47 @@
 package com.codington.festival.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.stereotype.Controller;
-import com.codington.festival.Models.*;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.codington.festival.Models.User;
 import com.codington.festival.Repositories.UserRepository;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	public UserRepository userRepo;
-	
+
 	public UserController(UserRepository userRepo) {
 		this.userRepo = userRepo;
 	}
-	
+
 	@GetMapping("/login")
 	public String showLoginform() {
 		return "login";
 	}
-	
 
 	@GetMapping("/register")
 	public String showRegistrationpage(Model model) {
 		model.addAttribute("user", new User());
 		return "register";
 	}
-	
+
 	@PostMapping("/register")
 	public String register(User user) {
 		userRepo.save(user);
 		return "success";
 	}
-	
+
 	@GetMapping("/profile")
 	public String showProfile(Model model) {
-		
+
 //		use userRepo or create service to get logged in users profile and add "user" as a model attribute
-		
+
 		return "profiles";
 	}
-	
-	
 
-	
-	
 }
-
